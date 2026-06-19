76ca45c3587bb013d29f0cb64b4993d1236dbab5
kchiang <kaizhi3006@gmail.com>
2026-06-20 03:13:46 +0800

    fix: added integer cast in fetch_tagged_jobs source_id comparision to be safe

--- sql/fetch_tagged_jobs.sql
+++ sql/fetch_tagged_jobs.sql
@@ -1,6 +1,6 @@
 SELECT source_id, tech_stack FROM jobs
 WHERE tech_stack IS NOT NULL
 AND TRIM(tech_stack) != ''
-AND source_id > last_sid
+AND CAST(source_id AS INTEGER) > last_sid
 ORDER BY source_id
 LIMIT :batch_size
